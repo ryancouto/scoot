3e394437394003837fefd6440641ab45eb4b9f3e
rcouto <rcouto@twitter.com>
2016-11-03 14:36:12 -0400

    scp stdout/stderr when scootapi get_job_status is invoked and job is completed

--- scootapi/client/get_status_cmd.go
+++ scootapi/client/get_status_cmd.go
@@ -3,9 +3,14 @@ package client
 import (
 	"errors"
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+
 	"github.com/scootdev/scoot/scootapi/gen-go/scoot"
 	"github.com/spf13/cobra"
-	"log"
 )
 
 type getStatusCmd struct{}
@@ -43,7 +48,48 @@ func (c *getStatusCmd) run(cl *simpleCLIClient, cmd *cobra.Command, args []strin
 		}
 	}
 
+	for _, runStatus := range status.GetTaskData() {
+		if runStatus.GetStatus() > 2 { // 0 - UNKNOWN, 1 - PENDING, 2 - RUNNING
+			c.saveStdOutAndErr(runStatus)
+		}
+	}
+
 	fmt.Println("Job Status: ", status)
 
 	return nil
+}
+
+func (c *getStatusCmd) saveStdOutAndErr(runStatus *scoot.RunStatus) {
+	runID, out, err := runStatus.GetRunId(), runStatus.GetOutUri(), runStatus.GetErrUri()
+	if _, err := os.Stat("~/scoot-std"); os.IsNotExist(err) {
+		os.Mkdir("~/scoot-std", 0777)
+	}
+	if _, err2 := os.Stat("~/scoot-std/" + runID); os.IsNotExist(err2) {
+		os.Mkdir("~/scoot-std"+runID, 0777)
+	}
+	c.saveStdStream(out, runID)
+	c.saveStdStream(err, runID)
+}
+
+func (c *getStatusCmd) saveStdStream(uri, runID string) {
+	uriSlice := strings.Split(uri, "://")
+	scheme := uriSlice[0]
+	hierPart := uriSlice[1]
+	switch scheme {
+	case "file":
+		c.scpFile(hierPart, runID)
+	default:
+		log.Fatal("Error resolving URI protocol")
+	}
+}
+
+func (c *getStatusCmd) scpFile(hierPart, runID string) {
+	re := regexp.MustCompile("([^://?#]*)?")
+	authority := re.FindString(hierPart)
+	filePath := strings.Split(authority, hierPart)[1]
+	scp := exec.Command("scp", authority+":"+filePath, "~/scoot-std/"+runID+"/")
+	err := scp.Run()
+	if err != nil {
+		log.Fatal("Error securely copying file: %v", err)
+	}
 }
