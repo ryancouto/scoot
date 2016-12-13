82af991e4a59b6752e78e210de8045d7b1233ae8
jeanette bruno <jbruno@twitter.com>
2016-12-13 15:11:08 -0500

    DX-1725 - adding dlv redirection script to enable using intellj debugger to debug unit tests, adding the required empty main() for the intellij unit test debugging configurations.

---
+++ tests/enable-intellij-test-debug.go
@@ -0,0 +1,12 @@
+package main
+
+// hack to enable intellij debugging of go tests
+// create a run application configuration with the following values:
+// File <full path to ~/workspace/src/github.com/scootdev/scootdev/tests/enable-intellij-test-debug.go>
+// Working Directory: <full path to ~/workspace/src/github.com/scootdev/scoot>
+// Program Arguments: test <full path to the test to debug>
+// Module: scoot
+
+func main() {
+
+}
